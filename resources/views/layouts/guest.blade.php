2baa6424b4588c4b84f3d5c0f02256f6050e486a
P41T0 <tortmasoliver@gmail.com>
2023-11-28 08:53:53 +0100

    canvis a la vista

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,7 +20,7 @@
                 <a href="/" >
                     <x-application-logo class="fill-current text-gray-500" />
                 </a>
-                <h1 class="font-bold w-full text-center text-6xl">P41 Technology S.L.</h1>
+                <h1 class="font-bold w-full text-center text-6xl">P41 Technology</h1>
             </div>
             <div class="bg-verd1">
                 <li class="flex p-2 text-top-center justify-center">
@@ -37,6 +37,7 @@
         <footer class="bg-verd4 pb-2">
             <p class="flex justify-center font-bold">{{__('Informació de contacte')}}</p>
             <div class="flex justify-center">
+                <p class="pl-2 pr-2">P41 Technology S.L.</p>
             <p class="pl-2 pr-2">Plaça Major 5, Roda de Ter</p>
             <p class="pl-2 pr-2">contact@p41t.com</p>
             <p class="pl-2 pr-2">+34 612 34 56 78</p>
