9a367ecb3e7c1c9f003d16173bf2de794d71fe5f
P41T0 <tortmasoliver@gmail.com>
2023-12-08 11:34:46 +0100

    c

--- resources/views/compres/detall.blade.php
+++ resources/views/compres/detall.blade.php
@@ -3,7 +3,8 @@
     <h3 class="text-center font-bold text-xl">productes comprats</h3>
     <form action="{{ route('actualiza.quantitat',[$comanda->id]) }}" method="POST">
         @csrf
-        <table>
+        <caption>{{_("Selecciona la quantitat del producte que vols entre 1 i 10 i guarda els canvis. Si selecciones 0, el producte s'eliminarà de la comanda.")}}</caption>
+        <table class=" border-verd5 border-solid border-5" border-style="solid">
             <tr>
                 <th>Nom Producte</th>
                 <th>Quantitat</th>
@@ -14,7 +15,7 @@
                 <tr>
                     <td>{{$prod->nom}}</td>
                     <td class="text-right">
-                        <input type="number" name="productes[{{$prod->id}}]" value="{{ old('productos.'.$prod->id, $prod->pivot->quantitat) }}" min="0">
+                        <input class="bg-verd4 rounded-lg m-2 border-none text-right" type="number" name="productes[{{$prod->id}}]" value="{{ old('productos.'.$prod->id, $prod->pivot->quantitat) }}" min="0" max="10" required>
                         @error('productos.'.$prod->id)
                             <span class="text-red-500">{{ $message }}</span>
                         @enderror
