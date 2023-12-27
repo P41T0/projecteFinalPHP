2c4bbe1d818ff1bd4537d9451c7561d316e91821
P41T0 <tortmasoliver@gmail.com>
2023-12-27 14:09:15 +0100

    formatacio

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/', [IniciController::class, 'index'])->name('inici');
 Route::get('/productes/{producte}', [IniciController::class, 'showProducte'])
     ->name('detall.producte');
@@ -42,49 +43,49 @@ Route::middleware('auth')->group(function () {
 
 // CRUD Botigues
 Route::get('/dashboard/botigues/{botiga}/edit', [App\Http\Controllers\BotigaController::class, 'edit'])
-->middleware(['auth','admin'])
-->name('botigues.edit');
+    ->middleware(['auth', 'admin'])
+    ->name('botigues.edit');
 Route::get('/dashboard/modificaBotigues', [App\Http\Controllers\BotigaController::class, 'index'])
-->middleware(['auth','admin'])
-->name('botigues.select');
+    ->middleware(['auth', 'admin'])
+    ->name('botigues.select');
 Route::put('/dashboard/botigues/{botiga}', [App\Http\Controllers\BotigaController::class, 'update'])
-    ->middleware(['auth','admin'])
+    ->middleware(['auth', 'admin'])
     ->name('botigues.update');
 //CRUD Productes
 Route::put('/dashboard/productes/{producte}', [App\Http\Controllers\ProducteController::class, 'update'])
-    ->middleware(['auth','admin'])
+    ->middleware(['auth', 'admin'])
     ->name('productes.update');
-    Route::get('/dashboard/modificaProductes', [App\Http\Controllers\ProducteController::class, 'index'])
-->middleware(['auth','admin'])
-->name('productes.select');
+Route::get('/dashboard/modificaProductes', [App\Http\Controllers\ProducteController::class, 'index'])
+    ->middleware(['auth', 'admin'])
+    ->name('productes.select');
 Route::get('/dashboard/creaProd', [App\Http\Controllers\ProducteController::class, 'create'])
-->middleware(['auth','admin'])
-->name('productes.create');
+    ->middleware(['auth', 'admin'])
+    ->name('productes.create');
 Route::put('/dashboard/storeProd', [App\Http\Controllers\ProducteController::class, 'store'])
-    ->middleware(['auth','admin'])
+    ->middleware(['auth', 'admin'])
     ->name('productes.store');
-Route::get('/dashboard/modifElems')->middleware(['auth','admin'])->name('modifElems');
+Route::get('/dashboard/modifElems')->middleware(['auth', 'admin'])->name('modifElems');
 Route::get('/dashboard/productes/{producte}/edit', [App\Http\Controllers\ProducteController::class, 'edit'])
-->middleware(['auth','admin'])
-->name('productes.edit');
+    ->middleware(['auth', 'admin'])
+    ->name('productes.edit');
 
 
 
 //CRUD seccions  
 Route::get('/dashboard/seccions/{seccio}/edit', [App\Http\Controllers\SeccioController::class, 'edit'])
-->middleware(['auth','admin'])
-->name('seccions.edit');    
+    ->middleware(['auth', 'admin'])
+    ->name('seccions.edit');
 Route::put('/dashboard/seccions/{seccio}', [App\Http\Controllers\SeccioController::class, 'update'])
-    ->middleware(['auth','admin'])
-    ->name('seccions.update');  
+    ->middleware(['auth', 'admin'])
+    ->name('seccions.update');
 Route::put('/dashboard/storeSeccio', [App\Http\Controllers\SeccioController::class, 'store'])
-    ->middleware(['auth','admin'])
+    ->middleware(['auth', 'admin'])
     ->name('seccions.store');
-    Route::get('/dashboard/modificaSeccions', [App\Http\Controllers\SeccioController::class, 'index'])
-->middleware(['auth','admin'])
-->name('seccions.select');
+Route::get('/dashboard/modificaSeccions', [App\Http\Controllers\SeccioController::class, 'index'])
+    ->middleware(['auth', 'admin'])
+    ->name('seccions.select');
 Route::get('/dashboard/creaSeccio', [App\Http\Controllers\SeccioController::class, 'create'])
-->middleware(['auth','admin'])
-->name('seccions.create');
+    ->middleware(['auth', 'admin'])
+    ->name('seccions.create');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
