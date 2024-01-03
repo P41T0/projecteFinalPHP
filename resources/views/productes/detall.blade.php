b52329fe70946877a5e10029dd3d571b7eea118e
P41T0 <tortmasoliver@gmail.com>
2024-01-03 19:05:18 +0100

    img

--- resources/views/productes/detall.blade.php
+++ resources/views/productes/detall.blade.php
@@ -3,7 +3,7 @@
         <div class="bg-verd2 max-w-4xl ml-auto mr-auto mt-12 mb-12 rounded-lg p-5 justify-center text-center">
         <h2 class="text-3xl text-center font-extrabold">{{$producte->nom}}</h2>
         <p>{{$producte->descripcio}}</p>
-        <img class="max-w-xl" src="https://www.p41t.com/storage/app/public/{{$producte->foto)}}" alt="{{$producte->nom}}">
+        <img class="max-w-xl" src="https://www.p41t.com/storage/app/public/{{$producte->foto}}" alt="{{$producte->nom}}">
         <p>{{__("Preu")}}: {{$producte->preu_unitari}}€</p>
         @if($producte->mostra_prod && $producte->seccio->mostra_sec)
         <a class=" hover:bg-verd4 p-2 m-2 rounded-sm" href="{{route('comprar', $producte->id)}}">{{__("Afegir a la llista de la compra")}}</a>
