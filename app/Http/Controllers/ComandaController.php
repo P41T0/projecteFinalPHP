1703e9ea2c870577ea8e4ea63973eacae8609b3c
P41T0 <tortmasoliver@gmail.com>
2024-01-13 22:10:12 +0100

    es comprova si el controlador de comanda ésunarray

--- app/Http/Controllers/ComandaController.php
+++ app/Http/Controllers/ComandaController.php
@@ -40,8 +40,8 @@ class ComandaController extends Controller
         }
         $usuari = Auth()->user()->id;
         $botiga = Botiga::all();
-       // dd($botiga);
-        return view('compres.detall',['numProductes'=>$numProds,'comanda'=>$comanda, 'preuTotal'=>$preuTotal, 'usuari'=>$usuari,'botigues'=>$botiga]);
+        // dd($botiga);
+        return view('compres.detall', ['numProductes' => $numProds, 'comanda' => $comanda, 'preuTotal' => $preuTotal, 'usuari' => $usuari, 'botigues' => $botiga]);
     }
     public function confirmar(Comanda $comanda)
     {
@@ -50,62 +50,63 @@ class ComandaController extends Controller
         $botigues = Botiga::find($comanda->botiga_id);
         $botiga = $botigues->poblacio;
         $usuari = Auth()->user()->id;
-        if ($comanda->usuari_id == $usuari){
-        if ($comanda->oberta != 1){
-            foreach ($comanda->productes as $prods) {
-                $preuTotal += $prods->preu_unitari * $prods->pivot->quantitat;
+        if ($comanda->usuari_id == $usuari) {
+            if ($comanda->oberta != 1) {
+                foreach ($comanda->productes as $prods) {
+                    $preuTotal += $prods->preu_unitari * $prods->pivot->quantitat;
+                }
+                $missatge = 0;
+            } else if ($comanda->productes->isEmpty()) {
+                $missatge = 1;
+            } else {
+                $comanda->oberta = 0;
+                $comanda->save();
+
+
+                foreach ($comanda->productes as $prods) {
+                    $preuTotal += $prods->preu_unitari * $prods->pivot->quantitat;
+                }
+                $missatge = 2;
             }
-            $missatge = 0;
-        }else if($comanda->productes->isEmpty()){
-            $missatge = 1;
-        }
-        else {
-            $comanda->oberta = 0;
-            $comanda->save();
-            
-            
-            foreach ($comanda->productes as $prods) {
-                $preuTotal += $prods->preu_unitari * $prods->pivot->quantitat;
-            }
-            $missatge = 2;
-        }
         } else {
             $missatge = 3;
         }
 
-        return view('compres.confirma', compact('comanda', 'missatge','preuTotal','botiga'));
+        return view('compres.confirma', compact('comanda', 'missatge', 'preuTotal', 'botiga'));
     }
 
 
     public function canviQuantitat(Request $request, Comanda $comanda)
     {
-        if($comanda->oberta==1){
-        $productes = $request->input('productes');
-        if($productes != NULL){
-        foreach ($productes as $producteId => $quantitat) {
-            if ($quantitat <= 0) {
-                $comanda->productes()->detach($producteId);
-            }else{
-                if ($quantitat==NULL){
-                    $quantitat=1;
+        if ($comanda->oberta == 1) {
+            $productes = $request->input('productes');
+            if ($productes != NULL && is_array($productes)) {
+                foreach ($productes as $producteId => $quantitat) {
+                    if ($quantitat <= 0) {
+                        $comanda->productes()->detach($producteId);
+                    } else {
+                        if ($producteId != null) {
+                            if ($quantitat == NULL) {
+                                $quantitat = 1;
+                            }
+                            if ($quantitat > 10) {
+                                $quantitat = 10;
+                            }
+                            $comanda->productes()->updateExistingPivot($producteId, ['quantitat' => $quantitat]);
+                        }
+                    }
                 }
-                if ($quantitat>10){
-                    $quantitat = 10;
-                }
-            $comanda->productes()->updateExistingPivot($producteId, ['quantitat' => $quantitat]);
+                $comanda->botiga_id = $request->input('botiga');
+                $comanda->save();
             }
+            if ($comanda->productes->isEmpty()) {
+                return redirect()->route('inici');
+            }
+            return redirect()->route('comprarNP');
+        } else {
+            return redirect()->route('confirma.compres', $comanda->id);
         }
-        $comanda->botiga_id=$request->input('botiga');
-        $comanda->save();
     }
-        if ($comanda->productes->isEmpty()) {
-            return redirect()->route('inici');
-        }
-        return redirect()->route('comprarNP');
-    }else{
-        return redirect()->route('confirma.compres',$comanda->id);
-    }
-}
     /**
      * Display a listing of the resource.
      */
